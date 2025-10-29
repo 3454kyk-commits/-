2ceada0eff24a29d66c455f5bc9ff8848bd3e4c9
3454kyk-commits <3454kyk@gmail.com>
2025-10-29 17:41:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ name= st.text_input('이름을 알려주세요!:')
 if st.button('환영인사'):
     st.write(name+'님 안녕하세요')
     st.balloons()
-    st.image('https://www.chosun.com/culture-life/k-culture/2025/09/25/YM2GSHRI2FBEDFXHPCFKF3ZBUU/')
+    st.image('https://www.chosun.com/resizer/v2/GGMF7OCIKAJ26C5SGJ23XZVOJU.jpg?auth=697d9fa0a5923b1d8c14816aa85807bfa0e3fd6a2b071acbe5a5c8ff60a064b3&width=616')
